a110260425e353f92c38a41de6438ce3682a2362
Kapui-M <kapuimorgank@gmail.com>
2023-02-20 03:03:16 -0800

    correction of file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,12 +12,11 @@ char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
- }
+}
 for (ch = 'A'; ch <= 'Z'; ch++)
 {
 putchar(ch);
- }     
+}     
 putchar('\n');
-
- return (0);
+return (0);
 }
