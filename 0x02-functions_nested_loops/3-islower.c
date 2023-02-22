3200c6e53a3a171fc0332a1c9bad954d90a8457b
Kapui-M <kapuimorgank@gmail.com>
2023-02-22 01:24:44 -0800

    addition of file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - returns boolean value
+ *
+ * Return:int (Success)
+ */
+int _islower(int r)
+{
+if (islower(r))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
 int putchar (int character);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
+int _putchar (int character);
+int islower(int c);
