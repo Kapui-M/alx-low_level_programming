f06857fa5463faea27807cf8d5f6725a7acd1b79
Kapui-M <kapuimorgank@gmail.com>
2023-02-21 21:01:31 -0800

    file modification

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+#include "main.h"
+int main()
 {
-printf("_putchar"\n);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
+
