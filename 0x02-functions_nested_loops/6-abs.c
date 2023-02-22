65387b78a7baf54242007daca43f495864328286
Kapui-M <kapuimorgank@gmail.com>
2023-02-22 10:59:00 -0800

    file committed

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * int_abs - calcululates abs r
+ * _abs - calcululates abs r
  * @r: abs param
- * 
+ *
  * Return:int (Success)
  */
 int _abs(int r)
 {
 int a;
-a=abs(r);
+a = abs(r);
 return (a);
 }
