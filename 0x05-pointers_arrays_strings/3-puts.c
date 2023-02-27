4706e310ec8295a8cdab94b7fa7c4ccccf9468cb
Kapui-M <kapuimorgank@gmail.com>
2023-02-27 05:15:25 -0800

    file added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - to print string
+ *
+ * @str : param to print
+ * Return:void success 
+ */
+void _puts(char *str)
+{
+printf("%s\n", str);
+return;
+}
