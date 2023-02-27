fb8dd9864c361f816588238fb995776c348fac4a
Kapui-M <kapuimorgank@gmail.com>
2023-02-27 03:27:55 -0800

    trailing space removed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * swap_int - integer values swap 
+ * swap_int - integer values swap
  * @a : first param
  * @b : second param
  */
 void swap_int(int *a, int *b)
 {
-*a = 42;
-*b = 98;
+*a = *b;
+*b = *a;
 return;
 }
